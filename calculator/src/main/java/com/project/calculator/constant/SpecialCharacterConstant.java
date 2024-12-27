ea04d2022a634481ca67e32ae3254f8e4314d17d
sonammatani <sonammatani2024@gmail.com>
2024-12-27 19:48:06 +0530

    added constant for special characters

---
+++ calculator/src/main/java/com/project/calculator/constant/SpecialCharacterConstant.java
@@ -0,0 +1,8 @@
+package com.project.calculator.constant;
+
+public class SpecialCharacterConstant {
+
+    public static final String BACK_SLASH= "//";
+    public static final String ESCAPE_LINE= "\n";
+    public static final String DELIMETER= ",";
+}
