7eff4a88446c23a59f79c0a0b0f29407b5ab2ae3
sonammatani <sonammatani2024@gmail.com>
2024-12-27 23:00:31 +0530

    modularized code and formatted code

--- calculator/src/main/java/com/project/calculator/service/CalculatorService.java
+++ calculator/src/main/java/com/project/calculator/service/CalculatorService.java
@@ -7,14 +7,22 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
 public class CalculatorService {
 
     public int add(String numbers) {
+
         if (StringUtil.isNullOrEmpty(numbers)) {
             return 0;
         }
 
+        numbers = replaceDelimiters(numbers);
+
+        String[] tokens = numbers.split(SpecialCharacterConstant.DELIMETER);
+
+        return calculateSum(tokens);
+    }
+
+    private String replaceDelimiters(String numbers) {
         String delimiter = SpecialCharacterConstant.DELIMETER;
         if (numbers.startsWith(SpecialCharacterConstant.BACK_SLASH)) {
             int delimiterIndex = numbers.indexOf(SpecialCharacterConstant.ESCAPE_LINE);
@@ -24,11 +32,12 @@ public class CalculatorService {
 
         numbers = numbers.replace(delimiter, SpecialCharacterConstant.DELIMETER);
         numbers = numbers.replace(SpecialCharacterConstant.ESCAPE_LINE, SpecialCharacterConstant.DELIMETER);
-        String[] tokens = numbers.split(SpecialCharacterConstant.DELIMETER);
+        return numbers;
+    }
 
+    private int calculateSum(String[] tokens) {
         List<Integer> negatives = new ArrayList<>();
         int sum = 0;
-
         for (String token : tokens) {
             int num = Integer.parseInt(token.trim());
             if (num < 0) {
@@ -41,7 +50,6 @@ public class CalculatorService {
         if (!negatives.isEmpty()) {
             throw new IllegalArgumentException("Negative numbers not allowed: " + negatives);
         }
-
         return sum;
     }
 
